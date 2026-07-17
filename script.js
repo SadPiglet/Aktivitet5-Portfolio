d3037e47d3869566be6d641720fa9809d41eb975
SadPiglet <nomail@nomail.com>
2026-07-17 14:00:05 +0200

    CSS fine-tuning, added back to top button

--- script.js
+++ script.js
@@ -53,3 +53,18 @@ let savedTheme = localStorage.getItem("chosen-theme");
 if (savedTheme) {
   setTheme(savedTheme);
 }
+
+// Back to top-knap
+let backToTopBtn = document.querySelector("#back-to-top");
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("visible"); 
+  } else {
+    backToTopBtn.classList.remove("visible");
+  }
+});
+
+backToTopBtn.addEventListener("click", function() {
+  window.scrollTo({ top:0, behavior: "smooth" });
+});
